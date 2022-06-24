4a203929d8b8b72952f12dee6166ff7ad7926d8a
vladimir <vladimirmawla2@gmail.com>
2022-06-24 17:18:54 +0300

    Adding removing users api

--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -1,32 +1,42 @@
-'use strict';
+"use strict";
 const express = require("express");
 const userModel = require("../models/userModel");
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
 const app = express();
 
 // Register API
 app.post("/register", async (request, response) => {
+  const user = new userModel(request.body);
+  user.hash_password = bcrypt.hashSync(request.body.password, 10);
 
-    const user = new userModel(request.body);
-    user.hash_password = bcrypt.hashSync(request.body.password, 10);
-    
-    try {
-        await user.save();
-        response.send(user);
-      } catch (error) {
-        response.status(500).send(error);
-      }
-  });
+  try {
+    await user.save();
+    response.send(user);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
 
 // Get All Users API
 app.get("/users", async (request, response) => {
-    const users = await userModel.find({});
-  
-    try {
-      response.send(users);
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  });
+  const users = await userModel.find({});
 
-  module.exports = app;
+  try {
+    response.send(users);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// Removing user API
+app.delete("/remove_user", async (req, res) => {
+  const users = await userModel.findOne({ _id: req.body }).deleteOne();
+
+  try {
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+})
+
+module.exports = app;
