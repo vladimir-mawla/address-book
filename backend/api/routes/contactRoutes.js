6c87c62fb4a246b29524fb73cc18b67800881b5f
vladimir <vladimirmawla2@gmail.com>
2022-06-24 19:32:15 +0300

    Creating remove contact api

--- backend/api/routes/contactRoutes.js
+++ backend/api/routes/contactRoutes.js
@@ -2,20 +2,29 @@
 const express = require("express");
 const Contact = require("../models/contactModel");
 const app = express();
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 // Add contact API
 app.post("/add_contact", async (request, response) => {
-    
-    const contact = new Contact(request.body);
+  const contact = new Contact(request.body);
 
-    try {
+  try {
     await contact.save();
     response.send(contact);
-    } catch (error) {
+  } catch (error) {
     response.status(500).send(error);
-    }
+  }
+});
+
+// Removing contact API
+app.delete("/remove_contact", async (req, res) => {
+  const contact = await Contact.findOne({ _id: req.body }).deleteOne();
+
+  try {
+    res.send(contact);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 
