ada24f34ea57d73b47b1c767293681e29e1f5055
vladimir <vladimirmawla2@gmail.com>
2022-06-24 18:48:22 +0300

    Returning user at login

--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -28,7 +28,7 @@ app.post('/login', async (req, res)  => {
     if (!user || !user.comparePassword(req.body.password)) {
       return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
     }
-    return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
+    return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs'), user });
   });
 });
 
