e9f3e50ac7f49586dec2eff100cde9f1249ee04b
vladimir <vladimirmawla2@gmail.com>
2022-06-24 18:48:00 +0300

    Adding contact schema

---
+++ backend/api/models/contactModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  RelationshipStatus: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: true,
+  },
+  location: {
+      long: {
+        type: Number,
+        unique: true,
+        trim: true,
+        required: true,
+      },
+      lat: {
+        type: Number,
+        unique: true,
+        trim: true,
+        required: true,
+      }
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+
+module.exports = contact;
+mongoose.model("Contact", ContactSchema);
