f4da257a5fc82745f1c085eb889f22f81a7e444b
vladimir <vladimirmawla2@gmail.com>
2022-06-25 22:45:25 +0300

    Importing contacts component to main page

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Contacts from './Contacts';
 
 const Page = () => {
     return (
         <div>
             <Navbar />
+            <Contacts />
         </div>
     )
 }
