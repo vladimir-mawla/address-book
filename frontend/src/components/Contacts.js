75c999a45c4de6f7e61b14f2971c6433056995fe
vladimir <vladimirmawla2@gmail.com>
2022-06-26 00:10:48 +0300

    Creating add contact function

--- frontend/src/components/Contacts.js
+++ frontend/src/components/Contacts.js
@@ -9,6 +9,29 @@ const Contacts = () => {
   const [contacts, setContacts] = useState([]);
   const user_id = localStorage.getItem("user_id");
 
+
+  function addContact() {
+    const contact_name = document.getElementById("contact-name");
+    const contact_email = document.getElementById("contact-email");
+    const contact_number = document.getElementById("contact-number");
+    const relation = document.getElementById("relation");
+    axios
+      .post("http://localhost:3000/add_contact", {
+        email: contact_email.value,
+        fullName: contact_name.value,
+        userId: user_id,
+        phoneNumber: contact_number.value,
+        RelationshipStatus: relation.value,
+      })
+      .then((res) => {
+        contact_email.value = "";
+        contact_name.value = "";
+        contact_number.value = "";
+        relation.value = "";
+      });
+  }
+
+
   useEffect(() => {
     axios
       .post("http://localhost:3000/get_contacts", {
@@ -40,6 +63,7 @@ const Contacts = () => {
       <strong>Full Name:</strong> <input id="contact-name"></input><br />
       <strong>Email:</strong> <input id="contact-email"></input><br />
       <strong>Phone Number:</strong> <input id="contact-number"></input><br />
+      <strong>Relationship:</strong> <input id="relation"></input><br />
       <Button text={"Add"} className={"add-contact"} onClick={() => { addContact();}}/>
       </div>
     </div>
