b899617f99a448b12037907194bc4e05b697575f
vladimir <vladimirmawla2@gmail.com>
2022-06-26 15:04:29 +0300

    Implementing the map function

--- frontend/src/components/AddLocation.js
+++ frontend/src/components/AddLocation.js
@@ -7,6 +7,40 @@ const DefaultZoom = 10;
 
 const App = () => {
 
+  const [defaultLocation, setDefaultLocation] = useState(DefaultLocation);
+
+  const [location, setLocation] = useState(defaultLocation);
+  const [zoom, setZoom] = useState(DefaultZoom);
+
+  function handleChangeLocation (lat, lng){
+    setLocation({lat:lat, lng:lng});
+  }
+  
+  function handleChangeZoom (newZoom){
+    setZoom(newZoom);
+  }
+
+  function handleResetLocation(){
+    setDefaultLocation({ ... DefaultLocation});
+    setZoom(DefaultZoom);
+  }
+
+  return (
+    <>
+  <button onClick={handleResetLocation}>Reset Location</button>
+  <label>Latitute:</label><input type='text' value={location.lat} disabled/>
+  <label>Longitute:</label><input type='text' value={location.lng} disabled/>
+  <label>Zoom:</label><input type='text' value={zoom} disabled/>
+  
+  <MapPicker defaultLocation={defaultLocation}
+    zoom={zoom}
+    mapTypeId="roadmap"
+    style={{height:'700px'}}
+    onChangeLocation={handleChangeLocation} 
+    onChangeZoom={handleChangeZoom}
+    apiKey='AIzaSyBnAOfaeDIrUzSanaPjpTgHSGbl3BG2cN0'/>
+  </>
+  );
 }
 
 export default App
